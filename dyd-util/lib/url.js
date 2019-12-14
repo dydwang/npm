9e3eb8038768a4bb2552e810171a36c530898e7d
DESKTOP-CHPE7CS\86186 <3010136674@qq.com>
2019-12-14 17:24:39 +0800

    md 使用方法

--- dyd-util/lib/url.js
+++
@@ -1,9 +0,0 @@
-const env=process.env.NODE_ENV  //判断模式
-/**
- * @development 本地开发模式
- * @production  服务器生产模式
- */
-const developmentNode='http://localhost:3000/' //本地NODE
-const productionNode='http://47.106.248.148:3000/' //服务器NODE
-
-export const serveUrl= env==='development'?developmentNode:productionNode //node默认路径，
